023b046fd5cdaaa8056056a57648da2ea6cf399c
Ivan Chinea <chitenavi@gmail.com>
2021-04-01 22:54:08 +0100

    Init: eslint and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'spaced-comment': 'off',
+    'no-console': 'warn',
+    'consistent-return': 'off',
+    'prefer-arrow-callback': 'error',
+    'func-names': 'off',
+    'object-shorthand': 'off',
+    'no-process-exit': 'off',
+    'no-param-reassign': 'off',
+    'no-return-await': 'off',
+    'no-underscore-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'prefer-destructuring': ['error', { object: true, array: false }],
+    'no-unused-vars': [
+      'error',
+      { argsIgnorePattern: 'req|res|next|val|reject' },
+    ],
+  },
+};
